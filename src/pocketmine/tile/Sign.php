2ae18ba514117a18757f8c30da0fd15ddc42c07c
tr <178709726+trrtren@users.noreply.github.com>
2025-03-08 14:41:08 +0000

    Update Sign.php

--- src/pocketmine/tile/Sign.php
+++ src/pocketmine/tile/Sign.php
@@ -56,9 +56,9 @@ class Sign extends Spawnable{
 	/**
 	 * @return string[]
 	 */
-	private static function fixTextBlob(string $blob) : array{
-		return array_slice(array_pad(explode("\n", $blob), 4, ""), 0, 4);
-	}
+	private static function fixTextBlob(string $blob) : array {
+    return array_slice(array_pad(explode("\n", $blob, 5), 4, ""), 0, 4);
+}
 
 	/** @var string[] */
 	protected $text = ["", "", "", ""];
